2bb92f38d4548400b44a5c1d3f2bf5e263aeaebd
mdtalhachy <talhachowdhury3.1416@gmail.com>
2020-09-12 22:04:21 +0600

    Added Register User function with Progress Dialog

---
+++ app/src/main/java/rocks/talha/instagram/RegisterActivity.java
@@ -0,0 +1,123 @@
+package rocks.talha.instagram;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private EditText username;
+    private EditText name;
+    private EditText email;
+    private EditText password;
+    private Button register;
+    private TextView loginUser;
+
+    ProgressDialog progressDialog;
+
+    private FirebaseAuth auth;
+    private DatabaseReference mRootRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        username = findViewById(R.id.username);
+        name = findViewById(R.id.name);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        register = findViewById(R.id.register);
+        loginUser = findViewById(R.id.login_user);
+
+        progressDialog = new ProgressDialog(this);
+
+        auth = FirebaseAuth.getInstance();
+        mRootRef = FirebaseDatabase.getInstance().getReference();
+
+        loginUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String txtUsername = username.getText().toString();
+                String txtName = name.getText().toString();
+                String txtEmail = email.getText().toString();
+                String txtPassword = password.getText().toString();
+
+                if(TextUtils.isEmpty(txtUsername) || TextUtils.isEmpty(txtName)
+                 || TextUtils.isEmpty(txtEmail) || TextUtils.isEmpty(txtPassword)){
+                    Toast.makeText(RegisterActivity.this, "Empty Credentials!", Toast.LENGTH_SHORT).show();
+                }else if(password.length() < 6){
+                    Toast.makeText(RegisterActivity.this, "Password too short!", Toast.LENGTH_SHORT).show();
+                }else{
+                    registerUser(txtUsername, txtName, txtEmail, txtPassword);
+                }
+
+            }
+        });
+    }
+
+    private void registerUser(final String username, final String name, final String email, String password) {
+        progressDialog.setMessage("Please Wait!");
+        progressDialog.show();
+
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                HashMap<String, Object> map = new HashMap<>();
+                map.put("name", name);
+                map.put("username", username);
+                map.put("email", email);
+                map.put("id", auth.getCurrentUser().getUid());
+
+                mRootRef.child("Users").child(auth.getCurrentUser().getUid()).setValue(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            //once the task is successful, dissmiss the progress dialog
+                            progressDialog.dismiss();
+                            Toast.makeText(RegisterActivity.this, "Update profile " +
+                                    "for better experience!", Toast.LENGTH_SHORT).show();
+
+                            Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                            startActivity(intent);
+                            finish();
+                        }
+                    }
+                });
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                progressDialog.dismiss();
+                Toast.makeText(RegisterActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
